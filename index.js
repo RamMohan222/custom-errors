5e14fcbe28ba0bc9519db5378445f9c722ae2f4c
RamMohan <ram222mohan@gmail.com>
2020-02-08 13:48:29 +0530

    custom error updated

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 // function base approch
 function CustomError(message, extra) {
-  // this.name = this.constructor.name
-  this.name = this.constructor.name;
-  this.message = message;
-  Error.captureStackTrace(this, this.constructor);
-  this.extra = extra;
+  var instance = new Error(message);
+  instance.name = 'CustomError';
+  instance.extra = extra;
+  Object.setPrototypeOf(instance, Object.getPrototypeOf(this));
+  if (Error.captureStackTrace) {
+    Error.captureStackTrace(instance, CustomError);
+  }
+  return instance;
 }
 
+CustomError.prototype = Object.create(Error.prototype, {
+  constructor: {
+    value: Error,
+    enumerable: false,
+    writable: true,
+    configurable: true
+  }
+});
+
+if (Object.setPrototypeOf){
+  Object.setPrototypeOf(CustomError, Error);
+} else {
+  CustomError.__proto__ = Error;
+}
 
 // class base approch
 class CustomError extends Error {
